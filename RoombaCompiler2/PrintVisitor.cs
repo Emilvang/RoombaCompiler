375b724a4b39de9f40ee498e29b81f02fcacfecb
Emilvang <Gravesenemil@hotmail.com>
2019-04-08 14:44:16 +0200

    Add project files.

---
+++ RoombaCompiler2/PrintVisitor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime.Misc;
+
+namespace RoombaCompiler2
+{
+    public class PrintVisitor : GrammarBaseVisitor<bool>
+    {
+        public override bool VisitProgram([NotNull] GrammarParser.ProgramContext context)
+        {
+            Console.WriteLine("Program");
+
+            return base.VisitProgram(context);
+        }
+
+        public override bool VisitVar_stmt([NotNull] GrammarParser.Var_stmtContext context)
+        {
+            Console.WriteLine("Var Statement");
+
+            return base.VisitVar_stmt(context);
+        }
+
+        public override bool VisitStmt([NotNull] GrammarParser.StmtContext context)
+        {
+            Console.WriteLine("Statement");
+
+            return base.VisitStmt(context);
+        }
+
+        public override bool VisitFunction_expr([NotNull] GrammarParser.Function_exprContext context)
+        {
+            Console.WriteLine("Expression");
+
+            return base.VisitFunction_expr(context);
+        }
+
+        public override bool VisitNum_expr([NotNull] GrammarParser.Num_exprContext context)
+        {
+            Console.WriteLine($"Numeric Expression {context.GetText()}");
+
+            return base.VisitNum_expr(context);
+        }
+    }
+}
---
+++ RoombaCompiler2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Antlr4.Runtime;
+
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoombaCompiler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var charStream = CharStreams.fromstring("Program { int Horse=2 Test = 55 }");
+
+            var lexer = new GrammarLexer(charStream);
+
+            var tokenStream = new CommonTokenStream(lexer);
+
+            var parser = new GrammarParser(tokenStream);
+
+            parser.BuildParseTree = true;
+
+            var tree = parser.program();
+
+            var visitor = new PrintVisitor();
+
+            visitor.VisitProgram(tree);
+
+            
+            Console.WriteLine("Works!");
+            Console.ReadLine();
+        }
+    }
+}
